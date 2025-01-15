27674f04d51d38e43b3584bfb9d317d17d8c4e28
Christian165293 <christian.bourquin05@gmail.com>
2025-01-15 11:16:42 -0600

    Finished app

--- app/src/test/java/org/example/HelloWorldTest.java
+++ app/src/test/java/org/example/HelloWorldTest.java
@@ -1,12 +1,13 @@
 package org.example;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HelloWorldTest {
-  @Test
-  void itHasAGreeting() {
-    HelloWorld helloWorld = new HelloWorld();
-    assertNotNull(helloWorld.getGreeting());
-  }
+    @Test
+    void itHasAGreeting() {
+        HelloWorld helloWorld = new HelloWorld();
+        assertNotNull(helloWorld.getGreeting());
+    }
 }
